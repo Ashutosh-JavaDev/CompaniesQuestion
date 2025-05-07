8dc6c5e8de2438b067bf9e96e88f0f98bd1a1233
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-08 01:05:01 +0530

    Java Interview DSA Practice Question

---
+++ src/SubArray.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class subarray{
+    public static void FindSum(int arr[],int size){
+        
+    }
+}
+public class SubArray {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
