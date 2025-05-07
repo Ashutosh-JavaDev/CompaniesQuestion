57d147ec3730c129a46a939ea18b296e5faa5066
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-08 01:19:15 +0530

    Java Interview DSA Practice Question

--- src/SubArray.java
+++ src/SubArray.java
@@ -18,6 +18,17 @@ class subarray{
 public class SubArray {
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the size of the Array");
+        int size=sc.nextInt();
+        int arr[]=new int[size];
+        System.out.println("Enter the value in the array");
+        try{
+            for(int i=0;i<size;i++){
+                arr[i]=sc.nextInt();
+            }
+        }catch(ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+        }
     }
 }
