c96dafcf26d0a0c44979f56d1212d594c4a7db7a
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-08 01:16:39 +0530

    Java Interview DSA Practice Question

--- src/SubArray.java
+++ src/SubArray.java
@@ -1,7 +1,18 @@
 import java.util.Scanner;
 class subarray{
-    public static void FindSum(int arr[],int size){
-        
+    public static boolean FindSum(int arr[],int size,int target){
+        boolean res=false;
+        for(int i=0;i<size;i++){
+            for(int j=i+1;j<size;j++){
+                if(arr[i]+arr[j]==target){
+                    System.out.println("value found at index: "+i);
+                    return true;
+                }
+            }
+            break;
+        }
+        return false;
+
     }
 }
 public class SubArray {
