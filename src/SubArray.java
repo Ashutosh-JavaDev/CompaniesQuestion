0e86c0003020c5f73b57b4d00ba93e7384070d87
Ashutosh Ayush <jbnashu297@gmail.com>
2025-05-08 01:20:19 +0530

    Java Interview DSA Practice Question

--- src/SubArray.java
+++ src/SubArray.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 class subarray{
     public static boolean FindSum(int arr[],int size,int target){
@@ -27,8 +28,12 @@ public class SubArray {
             for(int i=0;i<size;i++){
                 arr[i]=sc.nextInt();
             }
-        }catch(ArrayIndexOutOfBoundsException e){
+        }
+        catch(ArrayIndexOutOfBoundsException e){
             e.printStackTrace();
+        }
+        catch(InputMismatchException s){
+            s.printStackTrace();
         }
     }
 }
